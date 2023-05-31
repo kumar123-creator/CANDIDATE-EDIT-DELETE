eb48253677bc3e38705ca4e3c99edb01bc630920
kumar123-creator <132423913+kumar123-creator@users.noreply.github.com>
2023-05-31 12:17:01 +0530

    code updated

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,27 @@
 <script>
+  import 'bootstrap/dist/css/bootstrap.min.css';
   import { onMount } from "svelte";
   import { createEventDispatcher } from "svelte";
-  import 'bootstrap/dist/css/bootstrap.min.css';
 
   let jsonData = [];
   let tableVisible = false;
   let selectedCandidate = null;
   let editedCandidate = null;
+  let successMessage = "";
   let showSuccessMessage = false;
   let showDeleteConfirmation = false;
+  let candidateToDelete = null;
+  let showEditCandidatePopup = false;
+  let showAddCandidatePopup = false;
+  let showCVUploadPopup = false;
+  let cvFile = null;
+  let fileName = '';
+  let newCandidate = {
+    firstName: '',
+    surname: '',
+    email: '',
+    mobile: ''
+  };
 
   const dispatch = createEventDispatcher();
 
@@ -26,9 +39,11 @@
   function handleTitleClick(candidate) {
     selectedCandidate = candidate;
     editedCandidate = { ...selectedCandidate };
+    dispatch("showPopup");
   }
 
   async function saveCandidate() {
+    // Update the selectedCandidate with the editedCandidate data
     Object.assign(selectedCandidate, editedCandidate);
 
     // Find the index of the selectedCandidate in the jsonData array
@@ -38,22 +53,22 @@
     jsonData[index] = { ...selectedCandidate };
 
     // Send the updated candidate data to the API
+    const apiUrl = `https://api.recruitly.io/api/candidate?apiKey=TEST1236C4CF23E6921C41429A6E1D546AC9535E`;
+    const response = await fetch(apiUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(selectedCandidate)
+    });
 
-    showSuccessMessage = true;
-
-    closePopup();
-  }
-
-  function deleteCandidate() {
-    // Find the index of the selectedCandidate in the jsonData array
-    const index = jsonData.findIndex(candidate => candidate.id === selectedCandidate.id);
-
-    // Remove the selectedCandidate from the jsonData array
-    jsonData.splice(index, 1);
-
-    // Send the delete request to the API
-
-    showSuccessMessage = true;
+    if (response.ok) {
+      successMessage = "Your details have been successfully updated.";
+      showSuccessMessage = true;
+    } else {
+      // Handle the case where the update request fails
+      console.error("Failed to update candidate data.");
+    }
 
     closePopup();
   }
@@ -61,37 +76,164 @@
   function closePopup() {
     selectedCandidate = null;
     editedCandidate = null;
+    newCandidate = {
+      firstName: '',
+      surname: '',
+      email: '',
+      mobile: ''
+    };
+    showEditCandidatePopup = false;
+    showAddCandidatePopup = false;
+    showCVUploadPopup = false;
+  }
+
+  function showDeleteWarning(candidate) {
+    candidateToDelete = candidate;
+    showDeleteConfirmation = true;
+  }
+
+  function cancelDelete() {
     showDeleteConfirmation = false;
+    candidateToDelete = null;
+  }
+
+  async function deleteCandidate() {
+    // Remove the candidate from the jsonData array
+    jsonData = jsonData.filter(candidate => candidate.id !== candidateToDelete.id);
+
+    // Send the delete request to the API
+    const apiUrl = `https://api.recruitly.io/api/candidate/${candidateToDelete.id}?apiKey=TEST1236C4CF23E6921C41429A6E1D546AC9535E`;
+    const response = await fetch(apiUrl, {
+      method: 'DELETE'
+    });
+
+    if (response.ok) {
+      successMessage = "Candidate deleted successfully.";
+      showSuccessMessage = true;
+    } else {
+      // Handle the case where the delete request fails
+      console.error("Failed to delete candidate.");
+    }
+
+    showDeleteConfirmation = false;
+    candidateToDelete = null;
+  }
+
+  function editCandidate() {
+    showEditCandidatePopup = true;
+  }
+
+  function addCandidate() {
+    showAddCandidatePopup = true;
+  }
+
+  async function saveEditedCandidate() {
+    // Send the editedCandidate data to the API
+    const apiUrl = `https://api.recruitly.io/api/candidate?apiKey=TEST1236C4CF23E6921C41429A6E1D546AC9535E`;
+    const response = await fetch(apiUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(editedCandidate)
+    });
+
+    if (response.ok) {
+      successMessage = "Candidate details updated successfully.";
+      showSuccessMessage = true;
+
+      // Fetch the updated candidate list
+      await fetchData();
+    } else {
+      // Handle the case where the edit request fails
+      console.error("Failed to update candidate details.");
+    }
+
+    closePopup();
+  }
+
+  async function saveNewCandidate() {
+    // Send the newCandidate data to the API
+    const apiUrl = `https://api.recruitly.io/api/candidate?apiKey=TEST1236C4CF23E6921C41429A6E1D546AC9535E`;
+    const response = await fetch(apiUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newCandidate)
+    });
+
+    if (response.ok) {
+      successMessage = "New candidate added successfully.";
+      showSuccessMessage = true;
+
+      // Fetch the updated candidate list
+      await fetchData();
+    } else {
+      // Handle the case where the add request fails
+      console.error("Failed to add new candidate.");
+    }
+
+    closePopup();
+  }
+
+  function handleCVFileChange(event) {
+    cvFile = event.target.files[0];
+    fileName = cvFile.name;
+  }
+
+  async function saveCV() {
+    const formData = new FormData();
+    formData.append('cvFile', cvFile);
+
+    // Send the CV file data to the API
+    const apiUrl = `https://api.recruitly.io/api/candidatecv/upload?apiKey=TEST1236C4CF23E6921C41429A6E1D546AC9535E`;
+    const response = await fetch(apiUrl, {
+      method: 'POST',
+      body: formData
+    });
+
+    if (response.ok) {
+      successMessage = "CV file saved successfully.";
+      showSuccessMessage = true;
+
+      // Fetch the updated candidate list
+      await fetchData();
+    } else {
+      // Handle the case where the upload request fails
+      console.error("Failed to save CV file.");
+    }
+
+    closePopup();
   }
 </script>
 
 <style>
   .popup {
     position: fixed;
-    top: 0;
     left: 0;
+    top: 0;
     width: 100%;
     height: 100%;
     background-color: rgba(0, 0, 0, 0.5);
     display: flex;
     justify-content: center;
     align-items: center;
+    z-index: 9999;
   }
 
   .popup-content {
-    background-color: #ffffff;
-    padding: 40px;
-    border-radius: 10px;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-    max-width: 400px;
-    width: 90%;
-    text-align: center;
+    background-color: #fff;
+    padding: 20px;
+    border-radius: 4px;
+    max-width: 500px;
+    width: 100%;
   }
 
   .popup-content h1 {
     color: blue;
     font-size: 24px;
-    margin-bottom: 25px;
+    margin-bottom: 10px;
   }
 
   .popup-content p {
@@ -99,108 +241,118 @@
   }
 
   .popup-content input {
+    margin-bottom: 10px;
+    padding: 5px;
     width: 100%;
-    padding: 7px;
-    margin-bottom: 15px;
   }
 
   .popup-content button {
-    margin-top: 20px;
-  }
-
-  .success-popup {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0, 0, 0, 0.5);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-
-  .success-popup-content {
-    background-color: #ffffff;
-    padding: 40px;
-    border-radius: 10px;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-    max-width: 400px;
-    width: 90%;
-    text-align: center;
+    padding: 5px 10px;
   }
 </style>
 
-<main class="container mt-4">
-  {#if tableVisible}
-    <div class="table-responsive">
-      <table class="table table-striped">
-        <thead class="thead-light">
-          <tr>
-            <th>First Name</th>
-            <th>Surname</th>
-            <th>Email</th>
-            <th>Mobile</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {#each jsonData as candidate}
-            <tr>
-              <td>
-                <a href="#" on:click|preventDefault={() => handleTitleClick(candidate)}>{candidate.firstName}</a>
-              </td>
-              <td>{candidate.surname}</td>
-              <td>{candidate.email}</td>
-              <td>{candidate.mobile}</td>
-              <td>
-                <button class="btn btn-primary btn-sm" on:click|preventDefault={() => handleTitleClick(candidate)}>Edit</button>
-                <button class="btn btn-danger btn-sm" on:click|preventDefault={() => showDeleteConfirmation = true}>Delete</button>
-              </td>
-            </tr>
-          {/each}
-        </tbody>
-      </table>
-    </div>
-  {/if}
-</main>
+{#if tableVisible}
+  <button class="btn btn-primary" on:click|preventDefault={addCandidate}>Add Candidate</button>
+  <button class="btn btn-primary" on:click|preventDefault={editCandidate}>Edit Candidate</button>
+  <table class="table table-bordered">
+    <thead>
+      <tr>
+        <th>First Name</th>
+        <th>Surname</th>
+        <th>Email</th>
+        <th>Mobile</th>
+        <th>Actions</th>
+      </tr>
+    </thead>
+    <tbody>
+      {#each jsonData as candidate}
+        <tr>
+          <td>
+            <a href="#" on:click|preventDefault={() => handleTitleClick(candidate)}>{candidate.firstName}</a>
+          </td>
+          <td>{candidate.surname}</td>
+          <td>{candidate.email}</td>
+          <td>{candidate.mobile}</td>
+          <td>
+            <button class="btn btn-primary btn-sm" on:click|preventDefault={() => showCVUploadPopup = true}>CV Upload</button>
+            <button class="btn btn-danger btn-sm" on:click|preventDefault={() => showDeleteWarning(candidate)}>Delete</button>
+          </td>
+        </tr>
+      {/each}
+    </tbody>
+  </table>
+{/if}
 
 {#if selectedCandidate}
-<div class="popup">
-  <div class="popup-content">
-    <h1 style="color:blue;">Candidate Details</h1>
-    <p>First Name: <input type="text" class="form-control" bind:value={editedCandidate.firstName} placeholder="Enter first name" /></p>
-    <p>Surname: <input type="text" class="form-control" bind:value={editedCandidate.surname} placeholder="Enter surname" /></p>
-    <p>Email: <input type="text" class="form-control" bind:value={editedCandidate.email} placeholder="Enter email" /></p>
-    <p>Mobile: <input type="text" class="form-control" bind:value={editedCandidate.mobile} placeholder="Enter mobile" /></p>
-    <p>CV Upload: <input type="file" class="form-control-file" /></p>
-    <div>
-      <button class="btn btn-primary" on:click={saveCandidate}>Save</button>
-      <button class="btn btn-secondary" on:click={closePopup}>Close</button>
+  <div class="popup">
+    <div class="popup-content">
+      <h1>Edit Candidate</h1>
+      <p>First Name:</p>
+      <input type="text" bind:value={editedCandidate.firstName}>
+
+      <p>Surname:</p>
+      <input type="text" bind:value={editedCandidate.surname}>
+
+      <p>Email:</p>
+      <input type="text" bind:value={editedCandidate.email}>
+
+      <p>Mobile:</p>
+      <input type="text" bind:value={editedCandidate.mobile}>
+
+      <button class="btn btn-primary" on:click|preventDefault={saveEditedCandidate}>Save</button>
+      <button class="btn btn-secondary" on:click|preventDefault={closePopup}>Cancel</button>
     </div>
   </div>
-</div>
+{/if}
+
+{#if showAddCandidatePopup}
+  <div class="popup">
+    <div class="popup-content">
+      <h1>Add Candidate</h1>
+      <p>First Name:</p>
+      <input type="text" bind:value={newCandidate.firstName}>
+
+      <p>Surname:</p>
+      <input type="text" bind:value={newCandidate.surname}>
+
+      <p>Email:</p>
+      <input type="text" bind:value={newCandidate.email}>
+
+      <p>Mobile:</p>
+      <input type="text" bind:value={newCandidate.mobile}>
+
+      <button class="btn btn-primary" on:click|preventDefault={saveNewCandidate}>Save</button>
+      <button class="btn btn-secondary" on:click|preventDefault={closePopup}>Cancel</button>
+    </div>
+  </div>
 {/if}
 
 {#if showDeleteConfirmation}
-<div class="popup">
-  <div class="popup-content">
-    <h1 style="color:red;">Delete Confirmation</h1>
-    <p>Are you sure you want to delete this candidate?</p>
-    <div>
-      <button class="btn btn-danger" on:click={deleteCandidate}>Delete</button>
-      <button class="btn btn-secondary" on:click={closePopup}>Cancel</button>
+  <div class="popup">
+    <div class="popup-content">
+      <h1>Confirm Delete</h1>
+      <p>Are you sure you want to delete this candidate?</p>
+      <button class="btn btn-danger" on:click|preventDefault={deleteCandidate}>Delete</button>
+      <button class="btn btn-secondary" on:click|preventDefault={cancelDelete}>Cancel</button>
     </div>
   </div>
-</div>
+{/if}
+
+{#if showCVUploadPopup}
+  <div class="popup">
+    <div class="popup-content">
+      <h1>CV Upload</h1>
+      <input type="file" accept=".pdf,.doc,.docx" on:change={handleCVFileChange}>
+      <p>{fileName}</p>
+      <button class="btn btn-primary" on:click|preventDefault={saveCV}>Save</button>
+      <button class="btn btn-secondary" on:click|preventDefault={closePopup}>Cancel</button>
+    </div>
+  </div>
 {/if}
 
 {#if showSuccessMessage}
-<div class="success-popup">
-  <div class="success-popup-content">
-    <h1>Success!</h1>
-    <p>Your details have been successfully updated.</p>
-    <button class="btn btn-primary" on:click={() => showSuccessMessage = false}>Close</button>
+  <div class="alert alert-success" role="alert">
+    {successMessage}
   </div>
-</div>
 {/if}
+
